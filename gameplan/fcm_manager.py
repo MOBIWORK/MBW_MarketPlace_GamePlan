a7644ed33e5edfdb4c3e0fed0d95564ea1a10691
dev3.ekgis <dev3.ekgis@gmail.com>
2024-08-13 10:41:14 +0700

    fix bug

--- gameplan/fcm_manager.py
+++ gameplan/fcm_manager.py
@@ -4,26 +4,28 @@ import frappe
 from gameplan.utils import init_config_fcm_admin
 
 # Lấy giá trị từ database
-fcm_admin_values = {
-    'type': frappe.db.get_single_value('GP FCM Admin', 'type'),
-    'project_id': frappe.db.get_single_value('GP FCM Admin', 'project_id'),
-    'private_key_id': frappe.db.get_single_value('GP FCM Admin', 'private_key_id'),
-    'private_key': frappe.db.get_single_value('GP FCM Admin', 'private_key'),
-    'client_email': frappe.db.get_single_value('GP FCM Admin', 'client_email'),
-    'client_id': frappe.db.get_single_value('GP FCM Admin', 'client_id'),
-    'auth_uri': frappe.db.get_single_value('GP FCM Admin', 'auth_uri'),
-    'token_uri': frappe.db.get_single_value('GP FCM Admin', 'token_uri'),
-    'auth_provider_x509_cert_url': frappe.db.get_single_value('GP FCM Admin', 'auth_provider_x509_cert_url'),
-    'client_x509_cert_url': frappe.db.get_single_value('GP FCM Admin', 'client_x509_cert_url'),
-    'universe_domain': frappe.db.get_single_value('GP FCM Admin', 'universe_domain')
-}
+# fcm_admin_values = {
+#     'type': frappe.db.get_single_value('GP FCM Admin', 'type'),
+#     'project_id': frappe.db.get_single_value('GP FCM Admin', 'project_id'),
+#     'private_key_id': frappe.db.get_single_value('GP FCM Admin', 'private_key_id'),
+#     'private_key': frappe.db.get_single_value('GP FCM Admin', 'private_key'),
+#     'client_email': frappe.db.get_single_value('GP FCM Admin', 'client_email'),
+#     'client_id': frappe.db.get_single_value('GP FCM Admin', 'client_id'),
+#     'auth_uri': frappe.db.get_single_value('GP FCM Admin', 'auth_uri'),
+#     'token_uri': frappe.db.get_single_value('GP FCM Admin', 'token_uri'),
+#     'auth_provider_x509_cert_url': frappe.db.get_single_value('GP FCM Admin', 'auth_provider_x509_cert_url'),
+#     'client_x509_cert_url': frappe.db.get_single_value('GP FCM Admin', 'client_x509_cert_url'),
+#     'universe_domain': frappe.db.get_single_value('GP FCM Admin', 'universe_domain')
+# }
 
 # Kiểm tra nếu tất cả giá trị đều tồn tại và không rỗng
-if all(fcm_admin_values.values()):
-    fcm_admin_values['private_key'] = fcm_admin_values['private_key'].replace("\\n", "\n")
-    config_service_account_key = fcm_admin_values
-else:
-    config_service_account_key = init_config_fcm_admin()
+# if all(fcm_admin_values.values()):
+#     fcm_admin_values['private_key'] = fcm_admin_values['private_key'].replace("\\n", "\n")
+#     config_service_account_key = fcm_admin_values
+# else:
+#     config_service_account_key = init_config_fcm_admin()
+
+config_service_account_key = init_config_fcm_admin()
 
 # Khởi tạo Firebase Admin SDK với chứng chỉ
 cred = credentials.Certificate(config_service_account_key)
